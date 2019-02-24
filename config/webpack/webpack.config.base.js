fc61e3f4b1bb78b182fa5335e025bcd92fd121af
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2019-02-24 18:57:42 +0100

    Added favicon

--- config/webpack/webpack.config.base.js
+++ config/webpack/webpack.config.base.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const WebappWebpackPlugin = require('webapp-webpack-plugin');
 const { resolveFromRootPath } = require('../helpers');
 
 module.exports = merge(common, {
@@ -26,6 +27,26 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
+    new WebappWebpackPlugin({
+      logo: resolveFromRootPath('public', 'favicon', 'favicon.png'),
+      emitStats: false,
+      persistentCache: true,
+      inject: true,
+      background: '#fff',
+      title: 'React Chat',
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: resolveFromRootPath('public', 'index.html'),
